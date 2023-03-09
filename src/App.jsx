06694ec331ad29f1ce9fa5a4df00baabd0235ca9
TheingiMamoru <theingi.nyunt.win@gmail.com>
2023-03-09 17:13:26 +0630

    home

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import NavBar from './components/NavBar'
+import Home from './pages/Home'
 
 const App = () => {
   return (
-    <div className='bg-BodyCol m-0 p-0'>
+    //bg-color => didn't work
+    <div className='bg-bodyCol m-0 p-0'>  
       <div className='max-w-screen-lg mx-auto'>
         <NavBar/>
+        <Home/>
       </div>
     </div>
   )
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const NavBar = () => {
   return (
-    <div>
-        <nav className="bg-bodyCol px-2 sm:px-4 py-2.5 rounded">
+    <div className='sticky top-0'>
+        <nav className="bg-bodyCol px-2 sm:px-4 py-2.5 rounded ">
             <div className="container flex flex-wrap items-center justify-between mx-auto">
                 <a href="https://flowbite.com/" className="flex items-center">
                     <img src="../src/assets/img/logo.png" className="h-6 mr-3 sm:h-8"/>
@@ -14,7 +14,7 @@ const NavBar = () => {
                     <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clipRule="evenodd"></path></svg>
                 </button>
                 <div className="hidden w-full md:block md:w-auto" id="navbar-default">
-                    <ul className="flex flex-col items-center p-4 mt-4  rounded-lg md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-transparent">
+                    <ul className="flex flex-col items-center p-4 mt-4  rounded-lg md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-transparent bg-fullCol">
                         <li>
                             <a href="#" className="block py-2 pl-3 pr-4 text-para rounded bg-transparent md:p-0 hover:text-orange" aria-current="page">
                                 Home
